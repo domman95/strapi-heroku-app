6cf114f9e23aca4baf7306bb8a01ae3e1014e968
domman95 <dominiklasek95@gmail.com>
2022-01-10 12:40:25 +0100

    deleted subbackend director

--- backend/backend/config/admin.js
+++
@@ -1,5 +0,0 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', 'd94fe37fa6ccc0d54d0cf42336423170'),
-  },
-});
--- backend/backend/src/api/problem/services/problem.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * problem service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::problem.problem');
